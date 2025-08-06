f384762283366c85f6dd346e3406178d33f2c6ee
cesargiraldoa <cesargiraldopersonal@gmail.com>
2025-08-05 20:26:33 -0500

    Update db_connection.py

--- db_connection.py
+++ db_connection.py
@@ -1,20 +1,29 @@
 import pyodbc
+import streamlit as st
 
+@st.cache_resource
 def obtener_conexion():
-    server = 'dentisalud.database.windows.net'
-    database = 'dentisalud-db'
-    username = 'cesar'
-    password = 'AI4Dentisalud*'
-    driver = '{ODBC Driver 18 for SQL Server}'
+    try:
+        server = 'dentisalud.database.windows.net'
+        database = 'dentisalud-db'
+        username = 'cesar'
+        password = 'AI4Dentisalud*'
+        driver = '{ODBC Driver 18 for SQL Server}'
 
-    connection_string = (
-        f'DRIVER={driver};'
-        f'SERVER={server};'
-        f'DATABASE={database};'
-        f'UID={username};'
-        f'PWD={password};'
-        f'TrustServerCertificate=yes;'
-    )
+        connection_string = f'''
+            DRIVER={driver};
+            SERVER={server};
+            DATABASE={database};
+            UID={username};
+            PWD={password};
+            Encrypt=yes;
+            TrustServerCertificate=no;
+            Connection Timeout=30;
+        '''
 
-    conn = pyodbc.connect(connection_string)
-    return conn
+        conexion = pyodbc.connect(connection_string)
+        return conexion
+
+    except Exception as e:
+        st.error(f"Error al conectar con la base de datos: {e}")
+        return None
